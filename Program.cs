473475a88e99cffcee7b578ac9250bce4d181b06
Evan Goemer <evantheprogrammer2024@outlook.com>
2024-05-06 16:03:25 -0400

    Added The Following Opcodes NOP CLC SEC SBC [IMM]
    Added new opcodes to test program
    
    Cleaned up code

--- Program.cs
+++ Program.cs
@@ -6,20 +6,53 @@
         memory[0xFFFC] = 0x00;
         memory[0xFFFD] = 0x00;
 
+        /*
+            LDA #2
+            CLC
+            ADC #2
+            STA $0200
+
+            SEC
+            SBC #3
+            STA $0201
+
+            HLT
+        */
+
         // add 2+2 and put the result in memory at 0x200
         memory[0x0000] = 0xA9; // LDA #2
         memory[0x0001] = 0x02;
-        memory[0x0002] = 0x69; // ADC #2
-        memory[0x0003] = 0x02;
-        memory[0x0004] = 0x8D; // STA $0200
-        memory[0x0005] = 0x00;
-        memory[0x0006] = 0x02;
-        memory[0x0007] = 0x02; // HLT
+        memory[0x0002] = 0x18; // CLC
+        memory[0x0003] = 0x69; // ADC #2
+        memory[0x0004] = 0x02;
+        memory[0x0005] = 0x8D; // STA $0200
+        memory[0x0006] = 0x00;
+        memory[0x0007] = 0x02;
+
+        // subtract 3 from result of previous operation and put the result in memory at 0x201
+        memory[0x0008] = 0x38; // SEC
+        memory[0x0009] = 0xE9; // SBC #3
+        memory[0x000A] = 0x03;
+        memory[0x000B] = 0x8D; // STA $0201
+        memory[0x000C] = 0x01;
+        memory[0x000D] = 0x02;
+
+        // execute a NOP
+
+        memory[0x000E] = 0xEA;
+
+        // stop program
+        memory[0x000F] = 0x02; // HLT
+
         Emulator6502 emulator = new Emulator6502(memory);
         while (emulator.isRunning)
         {
             emulator.executeNextInstruction();
-            emulator.printDebugInfo();
+
+            if(emulator.debug)
+            { 
+                emulator.printDebugInfo(); 
+            }
         }
     }
 }
@@ -55,6 +88,26 @@ public class Emulator6502
                 {
                     printInstructionInfo();
                 }
+                break;
+            case 0x18: // CLC
+                if (debug)
+                {
+                    printInstructionInfo();
+                }
+
+                setCarryFlag(false);
+                pc++;
+
+                break;
+            case 0x38: // SEC
+                if (debug)
+                {
+                    printInstructionInfo();
+                }
+
+                setCarryFlag(true);
+                pc++;
+
                 break;
             case 0x69: // ADC #immediate
                 if (debug)
@@ -106,6 +159,42 @@ public class Emulator6502
                 pc++;
 
                 break;
+            case 0xE9: // SBC #immediate
+                if (debug)
+                {
+                    printInstructionInfo();
+                }
+
+                pc++;
+                value = memory[pc];
+                pc++;
+                result = (ushort)(a - value);
+                if (!carryFlag())
+                {
+                    result--;
+                }
+
+                if (result > 0xFF)
+                {
+                    setCarryFlag(true);
+                }
+                else
+                {
+                    setCarryFlag(false);
+                }
+
+                a = (byte)result;
+
+                break;
+            case 0xEA: // NOP
+                if (debug)
+                {
+                    printInstructionInfo();
+                }
+
+                pc++;
+
+                break;
             default:
                 pc++;
                 if (debug)
@@ -149,6 +238,7 @@ public class Emulator6502
         System.Console.WriteLine("Y: {0:X2}", y);
         System.Console.WriteLine("Status: {0:X2}", status);
         System.Console.WriteLine("0x0200: {0:X2}", memory[0x0200]);
+        System.Console.WriteLine("0x0201: {0:X2}", memory[0x0201]);
     }
 
     public void delayMs()
